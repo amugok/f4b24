aa34bee45296dfb1a9f4fff3eb553e4b6333a453
amugok <amugok@uniabis.net>
2008-06-25 00:58:03 +0000

    test21

--- source/fittle/src/fittle.cpp
+++ source/fittle/src/fittle.cpp
@@ -27,11 +27,11 @@
 // ソフト名（バージョンアップ時に忘れずに更新）
 #define FITTLE_VERSION TEXT("Fittle Ver.2.2.2 Preview 3")
 #ifdef UNICODE
-#define F4B24_VERSION_STRING TEXT("test20u")
+#define F4B24_VERSION_STRING TEXT("test21u")
 #else
-#define F4B24_VERSION_STRING TEXT("test20")
+#define F4B24_VERSION_STRING TEXT("test21")
 #endif
-#define F4B24_VERSION 20
+#define F4B24_VERSION 21
 #define F4B24_IF_VERSION 18
 #ifndef _DEBUG
 #define FITTLE_TITLE FITTLE_VERSION TEXT(" for BASS 2.4 ") F4B24_VERSION_STRING
