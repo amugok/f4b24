79c656f0c6ce22ce174e64c5752e7343232015b9
amugok <amugok@uniabis.net>
2008-12-19 01:26:48 +0000

    test42

--- trunk/bin/build.bat
+++ trunk/bin/build.bat
@@ -6,5 +6,5 @@ if exist "%ProgramFiles%\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT" CALL "%P
 
 :skipsetup
 
-perb /v "0811120A" fittle.exe
+perb /v "0812190A" fittle.exe
 editbin /release fittle.exe
