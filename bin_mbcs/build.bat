f68d3284265999a74c740bc02fc8829c86d30167
amugok <amugok@uniabis.net>
2017-06-29 11:54:13 +0900

    post build scripts

---
+++ bin_mbcs/build.bat
@@ -0,0 +1,43 @@
+@echo off
+
+if not "%VCINSTALLDIR%"=="" goto skipsetup
+if not "%MSVCDir%"=="" goto skipsetup
+
+if exist "%VS140COMNTOOLS%..\..\VC\Bin\vcvars32.bat" goto vc2015setup
+if exist "%VS100COMNTOOLS%..\..\VC\Bin\vcvars32.bat" goto vc2010setup
+if exist "%VS90COMNTOOLS%..\..\VC\Bin\vcvars32.bat" goto vc2008setup
+if exist "%VS80COMNTOOLS%..\..\VC\Bin\vcvars32.bat" goto vc2005setup
+if exist "%VS71COMNTOOLS%..\..\VC\Bin\vcvars32.bat" goto vc2003setup
+if exist "%ProgramFiles%\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT" goto vc6setup
+
+goto skipsetup
+
+:vc6setup
+CALL "%ProgramFiles%\Microsoft Visual Studio\VC98\Bin\VCVARS32.BAT"
+goto skipsetup
+
+:vc2003setup
+CALL "%VS71COMNTOOLS%..\..\VC\Bin\vcvars32.bat"
+goto skipsetup
+
+:vc2005setup
+CALL "%VS80COMNTOOLS%..\..\VC\Bin\vcvars32.bat"
+goto skipsetup
+
+:vc2008setup
+CALL "%VS90COMNTOOLS%..\..\VC\Bin\vcvars32.bat"
+goto skipsetup
+
+:vc2010setup
+CALL "%VS100COMNTOOLS%..\..\VC\Bin\vcvars32.bat"
+goto skipsetup
+
+:vc2015setup
+CALL "%VS140COMNTOOLS%..\..\VC\Bin\vcvars32.bat"
+goto skipsetup
+
+
+:skipsetup
+
+..\buildutil\perb /v "1210240A"  fittle.exe
+editbin /release fittle.exe
